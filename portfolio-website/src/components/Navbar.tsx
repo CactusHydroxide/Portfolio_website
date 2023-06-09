92141106dcb97da0b0414e6be2d7519c93b96737
cactushydroxie <clementloyme@gmail.com>
2023-06-09 21:09:39 +0800

    mobile nav with transition for height

--- portfolio-website/src/components/Navbar.tsx
+++ portfolio-website/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { GoListUnordered } from 'react-icons/go'
 
 const NavBar: FC = () => {
     const [navInView, setNavInView] = useState<boolean>(false)
+    const [navCollapse, setNavCollapse] = useState<boolean>(true)
     const [prevTop, setPrevTop] = useState<number>(0)
     const [currentTop, setCurrentTop] = useState<number>(0)
 
@@ -21,25 +22,27 @@ const NavBar: FC = () => {
         setPrevTop(currentTop)
     }, [currentTop])
 
+    let showCollapsedClassName = `${!navCollapse && styles.showCollapsedItem}`
+
     return (
-        <div className={`${styles.navbarContainer} ${navInView && styles.show}`}>
+        <div className={`${styles.navbarContainer} ${navInView && styles.show} ${showCollapsedClassName}`}>
             <div className={styles.logo}>
                 <h3>Clement Loy</h3>
             </div>
-            <ul className={styles.nav}>
+            <ul className={`${styles.nav}`}>
                 <li>
-                    <GoListUnordered className={styles.navListIcon} />
+                    <GoListUnordered className={styles.navListIcon} onClick={() => { setNavCollapse(current => !current) }} />
                 </li>
-                <li>
+                <li className={showCollapsedClassName}>
                     <p>About</p>
                 </li>
-                <li>
+                <li className={showCollapsedClassName}>
                     <p>Skills</p>
                 </li>
-                <li>
+                <li className={showCollapsedClassName}>
                     <p>Experience</p>
                 </li>
-                <li>
+                <li className={showCollapsedClassName}>
                     <p>Contact</p>
                 </li>
             </ul>
