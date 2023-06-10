7fcd30c0de77b7ee2f12628c6ed3b15510e81356
cactushydroxie <clementloyme@gmail.com>
2023-06-11 03:30:09 +0800

    contact

--- portfolio-website/src/components/Contact.tsx
+++ portfolio-website/src/components/Contact.tsx
@@ -1,22 +1,35 @@
 import { FC } from "react"
 import styles from "../styles/Contact.module.scss"
+import { FaGithub, FaEnvelope, FaLinkedinIn } from "react-icons/fa"
 
 const Contact: FC = () => {
     return (
         <div id={styles.contactContainer}>
             <div id={styles.contactLeft}>
-                <h2>Get it touch!</h2>
+                <h2>Get in touch!</h2>
                 <p>Looking to launch a website? Looking for a developer for your next big project? My inbox is always open for the next opportunity!</p>
-                <button>Email me!</button>
+                <button>Let's Chat!</button>
             </div>
             <div id={styles.contactRight}>
-                <h3>Socials</h3>
-                <ul>
-                    <li>Github</li>
-                    <li>Email</li>
-                    <li>LinkedIn (Coming Soon)</li>
-                    <li>Youtube (Coming Soon)</li>
-                </ul>
+                <div id={styles.squareRatio}></div>
+                <div id={styles.squareContent}>
+                    <h3>Socials</h3>
+                    <ul>
+                        <li>
+                            <FaGithub />
+                            <a>Github</a>
+                        </li>
+                        <li>
+                            <FaEnvelope />
+                            <a>Email</a>
+                        </li>
+                        <li>
+                            <FaLinkedinIn />
+                            <a>LinkedIn</a>
+                        </li>
+                        {/* <li>Youtube (Coming Soon)</li> */}
+                    </ul>
+                </div>
             </div>
         </div>
     )
