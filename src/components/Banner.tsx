396d5b9cb2e00af880319024b84289d3ed16fd2c
Clement Loy (CactusHydroxide) <clementloyme@gmail.com>
2023-06-11 19:08:38 +0800

    banner img animations

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,28 @@
 import { FC } from "react";
 import styles from '../styles/Banner.module.scss'
+import imgUrl from '../public/banner_img.jpg'
 
 const Banner: FC = () => {
     return (
         <div id={styles.bannerContainer}>
             <div id={styles.bannerImgContainer} >
-                <div className={`${styles.bannerImgBlock} ${styles.bannerBg1}`}></div>
-                <div className={`${styles.bannerImgBlock} ${styles.bannerBg2}`}></div>
-                <div className={`${styles.bannerImgBlock} ${styles.bannerImg}`}></div>
+                <div className={`${styles.bannerImgBlock} ${styles.bannerBg1}`}>
+                    <div className={styles.square}></div>
+                </div>
+                <div className={`${styles.bannerImgBlock} ${styles.bannerBg2}`}>
+                    <div className={styles.square}></div>
+                </div>
+                <div className={`${styles.bannerImgBlock} ${styles.bannerImg}`}>
+                    <div className={styles.square}>
+                        <img src={imgUrl} alt="My face" />
+                    </div>
+                </div>
+                <div className={styles.square}>
+                </div>
             </div>
 
             <div id={styles.bannerContent}>
-                <p>Hello, my name is</p>
+                <span>Hello, my name is</span>
                 <h1>Clement Loy</h1>
                 <p>Something something subtitle subtitle. noise noise something something profit</p>
             </div>
